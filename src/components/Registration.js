afd50cedf15c55470c87d013c6fa6d830ef306a7
stjepanB <stjepan.bencic95@gmail.com>
2020-12-01 12:10:54 +0100

    added registration

---
+++ src/components/Registration.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import PrivateRegistration from "./PrivateRegistration"
+import LegalRegistration from "./LegalRegistration"
+import { Paper, Radio, FormControl, FormControlLabel, RadioGroup} from '@material-ui/core';
+
+
+export default function Registration() {
+    const [selectedItem, setSelectedItem] = useState("privateUser")
+    return (
+        <div>
+
+        <FormControl component="fieldset" size="medium"  fullWidth>
+            <RadioGroup row aria-label='position' name='position'>
+                <FormControlLabel
+                            value="top"
+                            control={<Radio 
+                                    color="primary"
+                                    checked={selectedItem === 'privateUser'}
+                                    onChange={(e)=>{setSelectedItem("privateUser")}}
+                                     />}
+                            label="Privatni korisnici"
+                            labelPlacement="start"
+                />
+                <FormControlLabel
+                            value="top"
+                            control={<Radio 
+                                    color="primary"
+                                    checked={selectedItem === 'legalUser'}
+                                    onChange={(e)=>{setSelectedItem("legalUser")}}
+                                     />}
+                            label="Organizacije"
+                            labelPlacement="start"
+                />
+
+            </RadioGroup>
+        </FormControl>
+        
+       
+            {selectedItem === "privateUser" ?
+                <PrivateRegistration />
+                :  <LegalRegistration />
+            }
+        
+        </div>
+
+    );
+  }
