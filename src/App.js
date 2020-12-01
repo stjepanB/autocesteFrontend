14b7d58e25da8c203235de4a7d4d4fbd199ae550
stjepanB <stjepan.bencic95@gmail.com>
2020-12-01 12:11:51 +0100

    old code imported in project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import Navbar from "./components/Navbar"
+import SignInSide from "./components/SignInSide"
+import Registration from "./components/Registration"
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginMe = (bool) => {
+    setIsLoggedIn(bool)
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {
+      isLoggedIn ?
+        <Navbar />
+      : <Registration />  //<SignInSide login={loginMe}/>
+
+    }
     </div>
   );
 }
